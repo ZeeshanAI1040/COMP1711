34732da46d201da3157179a7c75739ec6f165bfa
ZeeshanAI1040 <146213549+ZeeshanAI1040@users.noreply.github.com>
2023-11-02 16:28:47 +0000

    Finished task (hopefully).

--- Assessment_Tasks/StepsTask1.c
+++ Assessment_Tasks/StepsTask1.c
@@ -4,27 +4,27 @@
 
 // Define an appropriate struct
 typedef struct {
-	char date[11];
-	char time[6];
-	int steps;
+    char date[11];
+    char time[6];
+    int steps;
 } FITNESS_DATA;
 
 // Define any additional variables here
-
 int num_of_lines;
-int buffer_size;
+int buffer_size = 24;
 
 // This is your helper function. Do not change it in any way.
 // Inputs: character array representing a row; the delimiter character
-// Ouputs: date character array; time character array; steps character array
+// Outputs: date character array; time character array; steps integer
 void tokeniseRecord(const char *input, const char *delimiter,
-                    char *date, char *time, char *steps) {
+                    char *date, char *time, int *steps) {
     // Create a copy of the input string as strtok modifies the string
     char *inputCopy = strdup(input);
     
     // Tokenize the copied string
     char *token = strtok(inputCopy, delimiter);
-    if (token != NULL) {        strcpy(date, token);
+    if (token != NULL) {
+        strcpy(date, token);
     }
     
     token = strtok(NULL, delimiter);
@@ -34,33 +34,46 @@ void tokeniseRecord(const char *input, const char *delimiter,
     
     token = strtok(NULL, delimiter);
     if (token != NULL) {
-        strcpy(steps, token);
+        *steps = atoi(token); // Convert the string to an integer
     }
     
     // Free the duplicated string
     free(inputCopy);
-
 }
 
 // Complete the main function
 int main() {
-
+    int num_of_records = 0;
+    char line_buffer[buffer_size];
+    
     FILE *csv_file = fopen("FitnessData_2023.csv", "r");
     if (csv_file == NULL) {
-        perror("");
+        perror("Error opening file");
         return 1;
     }
 
-    buffer_size = 24;
-    char line_buffer[buffer_size];
 
-    while (fgets(line_buffer, 24, csv_file) != NULL)
-    {
+    printf("Number of lines in the file: %d\n", num_of_lines);
+
+    while (num_of_records < 3 && fgets(line_buffer, sizeof(line_buffer), csv_file) != NULL) {
+        char date[11];
+        char time[6];
+        int steps;
+
+        // Parse the line into date, time, and steps
+        tokeniseRecord(line_buffer, ",", date, time, &steps);
+
+        // Print the formatted output
+        printf("%s/%s/%d\n", date, time, steps);
+
+        num_of_records++;
+    }
+
+    while (fgets(line_buffer, buffer_size, csv_file) != NULL) {
         num_of_lines++;
     }
 
-    printf("Number of lines in the file: %d\n", num_of_lines);
-    
     fclose(csv_file);
-
+    return 0;
 }
+
